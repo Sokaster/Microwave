4f496382a0f9a42d8a0dae2c106a05e18012705e
Sokaster <danenkov.dmitriy@list.ru>
2022-08-18 21:56:19 +0300

    FULL

---
+++ HomeWork10_MicroWaveSetup/Microwave.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork10_MicroWaveSetup
+{
+    public delegate void NotifyWarmUpCompleted(string nameOfFood);
+    internal class Microwave
+    {
+
+        public event NotifyWarmUpCompleted WarmUpCompleted;
+
+        public void WarmUp(string nameOfFood)
+        {
+            Console.WriteLine($"Подогреваю {nameOfFood}");
+            WarmUpCreated(nameOfFood);
+        }
+
+        protected virtual void WarmUpCreated(string nameOfFood)
+        {
+            if (WarmUpCompleted != null)
+            {
+                WarmUpCompleted.Invoke(nameOfFood);
+            }
+        }
+
+
+    }
+}
---
+++ HomeWork10_MicroWaveSetup/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeWork10_MicroWaveSetup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Microwave microwave = new Microwave();
+            microwave.WarmUp("Burger");
+            microwave.WarmUpCompleted += WarmUpCompletedHandler1;
+            microwave.WarmUp("Burger");
+            microwave.WarmUpCompleted += WarmUpCompletedHandler2;
+            microwave.WarmUp("Sandwich");
+            microwave.WarmUpCompleted += WarmUpCompletedHandler3;
+            microwave.WarmUp("Brokkoli");
+            microwave.WarmUpCompleted -= WarmUpCompletedHandler3;
+            microwave.WarmUp("potato");
+
+
+        }
+        private static void WarmUpCompletedHandler1(string nameOfFood)
+        {
+            Console.WriteLine($"Guest 1 Ваша еда готова: {nameOfFood}");
+        }
+
+        private static void WarmUpCompletedHandler2(string nameOfFood)
+        {
+            Console.WriteLine($"Guest 2 Ваша еда готова: {nameOfFood}");
+        }
+
+        private static void WarmUpCompletedHandler3(string nameOfFood)
+        {
+            Console.WriteLine($"Guest 3 Ваша еда готова: {nameOfFood}");
+        }
+    }
+}
